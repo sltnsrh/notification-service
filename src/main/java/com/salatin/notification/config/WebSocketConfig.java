44de857d6b2edea4652f28ee06ddb5e713622e09
Serhii Salatin <lifenjoy8888@gmail.com>
2023-08-04 11:49:46 +0300

    removed redundant topic config

--- src/main/java/com/salatin/notification/config/WebSocketConfig.java
+++ src/main/java/com/salatin/notification/config/WebSocketConfig.java
@@ -1,7 +1,6 @@
 package com.salatin.notification.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -15,9 +14,4 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws").withSockJS();
     }
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/notification-topic");
-    }
 }
